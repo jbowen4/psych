add7df498a81683993cd3a5a950494bfe4ef038d
Joshua Bowen <joshnbowen@gmail.com>
2021-07-18 11:59:33 -0400

    commit

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-// const cors = require("cors")
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
 
 const app = express();
-
-// app.use(cors())
-
 const server = http.createServer(app);
-const io = socketio(server);
-
-app.get('/', function (req, res) {
-    res.send('hello world')
-  })
+const io = socketio(server, {
+  cors: {
+    origin: ['http://localhost:3006']
+  }
+});
 
 // Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
+
+    socket.on("makeRoom", ({username, room}) => {
+      // Create new user
+      const user = userJoin(socket.id, username, room);
+
+      // Join
+      socket.join(room)
+
+      // Emit to user entered new room successfully
+      socket.emit("joinSuccess")
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    })
+
+    socket.on("joinRoom", ({username, room}) => {
+      // Create new user
+      const user = userJoin(socket.id, username, room);
+
+      var room = io.sockets.adapter.rooms[room]
+
+      // Check if room does not exist
+      if (room === undefined) {
+        socket.emit('joinFailure');
+        return
+      }
+
+      // Join
+      socket.join(roomId)
+      
+      // Emit to user entered new room successfully
+      socket.emit("joinSuccess")
+      
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    })
  });
  
  const PORT = 3000 || process.env.PORT;
